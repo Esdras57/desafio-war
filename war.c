a4c73b42b3ac64c39cf4b25b1998829f8636ecb4
Esdras <neemiasesdras05@gmail.com>
2025-09-24 20:21:18 -0300

    Estrutura do territorio criada

---
+++ war.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+// Constante Global
+
+#define MAX_TERRITORIOS 5
+
+// Estrutura (struct)
+struct Territorio
+{
+    char nome[30];
+    char cor[10];
+    int tropas;
+};
+
+int main()
+{
+}
